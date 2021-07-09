77c5d3962a68fc442d821bd63e7c018c35356941
Eason0729 <easonqq0000@gmail.com>
2021-07-09 21:53:26 +0800

    service worker fix

--- fe/serviceworker.js
+++ fe/serviceworker.js
@@ -1,6 +1,6 @@
 const config = {
     port: '3000',
-    last_edit: '1625837814592',// result form Date.now()
+    last_edit: '1625837814592', // result form Date.now()
 }
 self.addEventListener('install', (event) => {
     event.waitUntil(
@@ -17,28 +17,31 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
     const url = new URL(event.request.url)
+    // console.log(url.host)
     if (url.port != config.port)
         // if not under the port, send directly
         event.respondWith(Promise.resolve(fetch(event.request)))
-    else if (url.host != 'localhost' && url.protocol != 'https:')
+    else if (url.hostname != 'localhost' && url.protocol == 'http:')
         // if browser unsupport, send directly
         event.respondWith(Promise.resolve(fetch(event.request)))
     else {
         //if under port cache request or send existed cache
-        caches.match(event.request).then((response) => {
-            if (!response) {
-                // console.log(`from Internet( ${event.request.url} )`)
-                caches.open('v1').then((cache) => {
-                    fetch(url).then(function (response) {
-                        if (!response.ok)
-                            throw new TypeError('Bad response status')
-                        return cache.put(url, response)
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                if (!response) {
+                    console.log(`from Internet( ${event.request.url} )`)
+                    caches.open('v1').then((cache) => {
+                        fetch(url).then(function (response) {
+                            if (!response.ok)
+                                throw new TypeError('Bad response status')
+                            return cache.put(url, response)
+                        })
                     })
-                })
-            } else {
-                // console.log(`from cache( ${event.request.url} )`)
-                return response
-            }
-        })
+                } else {
+                    console.log(`from cache( ${event.request.url} )`)
+                    return response
+                }
+            })
+        )
     }
 })
